213c0f24d6dd8b066506447bec49e8590f47d1b6
engin üstün <enginustun@outlook.com>
2018-09-05 21:08:16 +0300

    Add react-app-rewired package and .sass support

--- src/Pages/ConnectedPage/ConnectedPage.js
+++ src/Pages/ConnectedPage/ConnectedPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { loadRemoteData, loadedRemoteData } from '../../actions';
-import '../Pages.css';
+import '../Pages.scss';
 
 class ConnectedPage extends Component {
   constructor(props) {
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import '../Pages.css';
+import '../Pages.scss';
 
 class HomePage extends Component {
   render() {
